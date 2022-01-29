17c1f381e19d84d83d6b823c4926a5a74d04a65a
wang-hsoo <dhkd4873@naver.com>
2022-01-30 01:19:35 +0900

    commit

--- xampp/htdocs/web/php/diary.php
+++ xampp/htdocs/web/php/diary.php
@@ -3,9 +3,8 @@
 <?php
 
     include('db.php');
-    include('server.php');
 
-    if( isset($_POST['title']) && isset($_POST['diary']) ){
+    if( !empty($_POST['title']) && !empty($_POST['diary']) ){
 
         $title = mysqli_real_escape_string($conn, $_POST['title']);
         $diary = mysqli_real_escape_string($conn, $_POST['diary']);
@@ -17,11 +16,39 @@
 
         $result = mysqli_query($conn, $sql_save);
 
-       
+        ?>
+        <script>
+            diaryArray = new Array();
+            diaryArray = JSON.parse(localStorage.getItem("diary"));
+
+            var diary = {
+                            title : "<?=$title?>",
+                            content : "<?=$diary?>",
+                            date : "<?=$date?>"
+                         }
+
+            if(diaryArray === null){
+
+                localStorage.setItem( "diary", JSON.stringify(diary));
+                listUp();
+                location.replace("../mainCon.html");
+
+            }else {
+                diaryArray.push(diary);
+                localStorage.setItem( "diary", JSON.stringify(diaryArray));
+                location.replace("../mainCon.html");
+            }
+        </script>
+       <?php
 
     }else{
         //그냥 에러
-       
+        
+       ?>
+       <script>
+            location.replace("../mainCon.html");
+       </script>
+       <?php
     }
 
 
