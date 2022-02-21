0c8942cd09dd751cbe69768d32e6688ac932fcbf
wang-hsoo <dhkd4873@naver.com>
2022-02-21 13:34:17 +0900

    commit

--- Users/user/react-web/src/App.js
+++ Users/user/react-web/src/App.js
@@ -1,11 +1,39 @@
 import Button from "./Button";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import { useState , useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  
+  console.log("i run all time");
+
+  
+  useEffect( () => {
+    console.log("i run only once");
+  }
+  , []);
+
+  useEffect( () => {
+    console.log("Search For", keyword)
+  }, [keyword]);
+  // [] keyword 가 변화할때 코드를 실행
+
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
   return (
     <div> 
-      <h1 className={styles.title}>Welcome</h1>
-      <Button text = {"continue"}/>
+      <input 
+        value = {keyword}
+        type="text" 
+        placeholder = "Search here" 
+        onChange={onChange} 
+        />
+
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick = {onClick}>click me </button>
     </div>
 
   );
